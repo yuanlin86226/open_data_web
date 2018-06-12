71964a9a593beb30b956eaed5a8205bf79ab7a14
YuanLin <sonia86226@gmail.com>
2018-06-12 16:50:19 +0800

    home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,115 +6,163 @@
 		<div class="container">
 			<div class="row top-line animate-box">
 				<div class="col-md-7 col-md-push-5 text-left intro">
-					<h2>Shift is a free html5 template to showcase your work. <span class="fh5co-highlight">Made with <i class="icon-heart2"></i> by <a href="http://freehtml5.co">FreeHTML5.co</a> </span></h2>
+					<h2 style="font-size: 35px;font-family: 'cwTeXFangSong', serif;">想遠離沙發 當個暫時的小文青 <span class="fh5co-highlight" style="color:#a9a9a9">Made by yuanlin <i class="icon-heart2" style="color:#bdbdbd;"></i></span></h2>
 					<!-- <h2>Shift is a Collection of a Beautiful &amp; Premium Themes.</h2> -->
 				</div>
 				
 			</div>
 			<div class="row">
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-1.jpg);">
+					<a class="work" href="about">
+						<div class="work-grid" style="background-image:url(images/coffee.jpg);">
 							<div class="inner">
 								<div class="desc">
-								<h3>Folding Light</h3>
-								<span class="cat">Branding</span>
+								<h3>Wellcome to Hipster</h3>
+								<span class="cat">小文青</span>
 							</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-2.jpg);">
+					<a class="work" href="work?keyword=bookstore">
+						<div class="work-grid" style="background-image:url(images/library-1.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Power Led Light</h3>
-									<span class="cat">Illustration</span>
+								<h3>Independent Bookstore</h3>
+								<span class="cat">獨立書店</span>
+							</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="work?keyword=library">
+						<div class="work-grid" style="background-image:url(images/library-2.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Featured Library</h3>
+									<span class="cat">特色圖書館</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-3.jpg);">
+					<a class="work" href="work?keyword=space">
+						<div class="work-grid" style="background-image:url(images/exhibition-1.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Elephant Origami</h3>
-									<span class="cat">Logo</span>
+									<h3>Exhibition Space</h3>
+									<span class="cat">展演空間</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-8 text-center animate-box">
+					<a class="work" href="work?keyword=course">
+						<div class="work-grid" style="background-image:url(images/library-3.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Ministry of Culture Study Course</h3>
+									<span class="cat">文化部研習課程</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-4.jpg);">
+					<a class="work" href="work?keyword=space">
+						<div class="work-grid" style="background-image:url(images/exhibition-2.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Electric Kick Scooter</h3>
-									<span class="cat">Web Design</span>
+									<h3>Exhibition Space</h3>
+									<span class="cat">展演空間</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-5.jpg);">
+					<a class="work" href="work?keyword=exhibition">
+						<div class="work-grid" style="background-image:url(images/art-exhibition-1.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Shark Diver</h3>
-									<span class="cat">Branding</span>
+									<h3>Exhibition Information</h3>
+									<span class="cat">展演資訊</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-6.jpg);">
+					<a class="work" href="work?keyword=exhibition">
+						<div class="work-grid" style="background-image:url(images/art-exhibition-2.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Objekte Unserer Tage Berlin</h3>
-									<span class="cat">Logo</span>
+									<h3>Exhibition Information</h3>
+									<span class="cat">展演資訊</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-8 text-center animate-box">
+					<a class="work" href="work?keyword=performance">
+						<div class="work-grid" style="background-image:url(images/piano-1.jpg);">
+							<div class="inner">
+								<div class="desc">
+								<h3>Musical performance information</h3>
+								<span class="cat">音樂表演資訊</span>
+							</div>
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center animate-box">
+					<a class="work" href="work?keyword=music">
+						<div class="work-grid" style="background-image:url(images/mic-1.jpg);">
+							<div class="inner">
+								<div class="desc">
+									<h3>Independent Music</h3>
+									<span class="cat">獨立音樂</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-7.jpg);">
+					<a class="work" href="work?keyword=performance">
+						<div class="work-grid" style="background-image:url(images/piano-2.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Aloha Shirt</h3>
-									<span class="cat">Web Design</span>
+									<h3>Musical performance information</h3>
+									<span class="cat">音樂表演資訊</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-8.jpg);">
+					<a class="work" href="work?keyword=performance">
+						<div class="work-grid" style="background-image:url(images/piano-3.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Olafur Elliasson Waterfall Versailles</h3>
-									<span class="cat">Branding</span>
+									<h3>Musical performance information</h3>
+									<span class="cat">音樂表演資訊</span>
 								</div>
 							</div>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail">
-						<div class="work-grid" style="background-image:url(images/project-9.jpg);">
+					<a class="work" href="work?keyword=music">
+						<div class="work-grid" style="background-image:url(images/mic-2.jpg);">
 							<div class="inner">
 								<div class="desc">
-									<h3>Hand Care</h3>
-									<span class="cat">Logo</span>
+									<h3>Independent Music</h3>
+									<span class="cat">獨立音樂</span>
 								</div>
 							</div>
 						</div>
@@ -123,7 +171,6 @@
 			</div>
 		</div>
 	</div>
-	
 
 @stop
 
