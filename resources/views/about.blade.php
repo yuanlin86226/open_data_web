5b0ac6681af07871ea1137d1d0b8da1315a941b6
YuanLin <sonia86226@gmail.com>
2018-06-16 17:58:25 +0800

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,110 +6,131 @@
 		<div class="container">
 			<div class="row top-line animate-box">
 				<div class="col-md-6 col-md-offset-3 col-md-push-2 text-left fh5co-heading">
-					<h2>About Me</h2>
-					<h3>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h3>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+					<h2>小文青</h2>
+					<h3 style="font-size: 15px;font-weight: 400;">不是不想出門，是不知道要去哪</a></h3>
+					<p style="font-size: 18px;">難得休假還跟你的枕頭難分難捨嗎？邊緣人、不知道去哪？<br>不要找理由！你們需要的資訊都在這裡了，給自己機會出門走走吧~</p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
 					<div class="author">
-						<div class="author-inner animate-box" style="background-image: url(images/person3.jpg);">
+						<div class="author-inner animate-box" style="background-image: url(images/library.jpg);">
 						</div>
 						<div class="desc animate-box">
-							<span>Web Designer &amp; Developer</span>
-							<h3>Mike Airways</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<p>Far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble"></i></a></li>
-								<li><a href="#"><i class="icon-github"></i></a></li>
-							</ul>
+							<span>Books &amp; Course</span>
+							<h3>文書課程</h3>
+							<p>有沒有一本書多年前讀過，多年後的某個瞬間又突然想起？</p>
+							<p>圖書館不單單只是保存書籍的地方，那裡也有著我們的青春回憶。學生時期，那些一起到圖書館報到、一起夜讀的日子，你還記得嗎？</p>
+							<p>看著別人多才多藝，是否有些羨慕？</p>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<h3 class="title animate-box">My Services</h3>
-						</div>
-						<div class="col-md-6 col-sm-6">
+					<div class="row text-center">
+						<div class="col-md-4 col-sm-4">
 							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
 									<i class="icon-brush"></i>
 								</span>
-								<h3>Web Design</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-								<p><a href="#" class="btn btn-primary btn-outline">Learn More</a></p>
+								<h3>獨立書店</h3>
+								<p style="height: 130px;">有沒有一本書多年前讀過，多年後的某個瞬間又突然想起？<br>每本書在不同年紀看都能看到不同的樣貌，到書店找尋那本可以陪伴你多年的書吧！</p>
+								<p><a href="/work?keyword=bookstore" class="btn btn-primary btn-outline">Learn More</a></p>
 							</div>
 						</div>
-						<div class="col-md-6 col-sm-6">
+						<div class="col-md-4 col-sm-4">
 							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
 									<i class="icon-cog"></i>
 								</span>
-								<h3>Web Development</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-								<p><a href="#" class="btn btn-primary btn-outline">Learn More</a></p>
+								<h3>特色圖書館</h3>
+								<p style="height: 130px;">圖書館不單單只是保存書籍的地方，那裡也有著我們的青春回憶<br>學生時期，那些一起到圖書館報到、一起夜讀的日子，你還記得嗎？</p>
+								<p><a href="/work?keyword=library" class="btn btn-primary btn-outline">Learn More</a></p>
 							</div>
 						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="feature-center animate-box" data-animate-effect="fadeIn">
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
+									<i class="icon-cog"></i>
+								</span>
+								<h3>文化部研習課程</h3>
+								<p style="height: 130px;">看著別人多才多藝，是否有些羨慕？<br>快來看看文化部有舉辦哪些課程吧</p>
+								<p><a href="/work?keyword=course" class="btn btn-primary btn-outline">Learn More</a></p>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</div>
 
+			<hr style = "margin-bottom: 100px;">
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="author">
+						<div class="author-inner animate-box" style="background-image: url(images/exhibition.jpg);">
+						</div>
+						<div class="desc animate-box">
+							<span>Exhibition</span>
+							<h3>展演資訊及空間</h3>
+							<p>喜歡看別人做的文青小物嗎？<br>到各個展覽空間去走走吧，說不定會有不一樣的新收穫喔</p>
+							<p>看看別人做的展覽，試著用他的角度去看世界<br>你會發現這個世界變得不同，用眼去看用心體會</p>
+						</div>
+					</div>
+					<div class="row text-center">
 						<div class="col-md-6 col-sm-6">
 							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
-									<i class="icon-briefcase"></i>
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
+									<i class="icon-brush"></i>
 								</span>
-								<h3>Branding</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-								<p><a href="#" class="btn btn-primary btn-outline">Learn More</a></p>
+								<h3>展演空間</h3>
+								<p style="height: 160px;width: 200px;margin:  0 auto;">喜歡看別人做的文青小物嗎?<br>到各個展覽空間去走走吧，說不定會有不一樣的新收穫喔</p>
+								<p><a href="/work?keyword=space" class="btn btn-primary btn-outline">Learn More</a></p>
 							</div>
 						</div>
 						<div class="col-md-6 col-sm-6">
 							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
-									<i class="icon-camera"></i>
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
+									<i class="icon-cog"></i>
 								</span>
-								<h3>Photography</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-								<p><a href="#" class="btn btn-primary btn-outline">Learn More</a></p>
+								<h3>展演資訊</h3>
+								<p style="height: 160px;width: 200px;margin:  0 auto;">看看別人做的展覽，試著用他的角度去看世界<br>你會發現這個世界變得不同，用眼去看用心體會</p>
+								<p><a href="/work?keyword=exhibition" class="btn btn-primary btn-outline">Learn More</a></p>
 							</div>
 						</div>
+					</div>
+				</div>
+			</div>
 
-						<div class="col-md-12">
-							<h3 class="title animate-box">My Skills</h3>
-							<div class="row">
-								<div class="col-md-6 animate-box skills">
-									<h3>Designing</h3>
-									<div class="progress">
-									   <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
-									      100%
-									   </div>
-									</div>
-								</div>
-								<div class="col-md-6 animate-box skills">
-									<h3>HTML5/CSS3</h3>
-									<div class="progress">
-									   <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width:95%">
-									      95%
-									   </div>
-									</div>
-								</div>
-								<div class="col-md-6 animate-box skills">
-									<h3>jQuery</h3>
-									<div class="progress">
-									   <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%">
-									      80%
-									   </div>
-									</div>
-								</div>
-								<div class="col-md-6 animate-box skills">
-									<h3>WordPress</h3>
-									<div class="progress">
-									   <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
-									      90%
-									   </div>
-									</div>
-								</div>
+			<hr style = "margin-bottom: 100px;">
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="author">
+						<div class="author-inner animate-box" style="background-image: url(images/guitar.jpg);">
+						</div>
+						<div class="desc animate-box">
+							<span>Music &amp; performance</span>
+							<h3>音樂及表演</h3>
+							<p>是否常在音樂的歌詞或旋律裡找自己的身影<br>在創作表演者的詮釋下，或許會發現類似的經歷與不同的選擇</p>
+							<p>有別於一般的人聲流行音樂<br>純音樂的饗宴，用最純淨的聲音，帶給你最震撼的感動</p>
+						</div>
+					</div>
+					<div class="row text-center">
+						<div class="col-md-6 col-sm-6">
+							<div class="feature-center animate-box" data-animate-effect="fadeIn">
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
+									<i class="icon-brush"></i>
+								</span>
+								<h3>獨立音樂</h3>
+								<p style="height: 160px;width: 200px;margin:  0 auto;">是否常在音樂的歌詞或旋律裡找自己的身影<br>在創作表演者的詮釋下，或許會發現類似的經歷與不同的選擇</p>
+								<p><a href="/work?keyword=music" class="btn btn-primary btn-outline">Learn More</a></p>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="feature-center animate-box" data-animate-effect="fadeIn">
+								<span class="icon" style="margin: 0 auto; margin-bottom: 30px;">
+									<i class="icon-cog"></i>
+								</span>
+								<h3>音樂表演資訊</h3>
+								<p style="height: 160px;width: 200px;margin:  0 auto;">有別於一般的人聲流行音樂<br>純音樂的饗宴，用最純淨的聲音，帶給你最震撼的感動</p>
+								<p><a href="/work?keyword=performance" class="btn btn-primary btn-outline">Learn More</a></p>
 							</div>
 						</div>
 					</div>
